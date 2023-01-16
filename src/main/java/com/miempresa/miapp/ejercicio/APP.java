e869b68cf077cb953ca320c73c3731a6b68c0aaf
LopezF <flv1980@gmail.com>
2023-01-16 18:18:00 +0100

    ejercicio de variables

---
+++ src/main/java/com/miempresa/miapp/ejercicio/APP.java
@@ -0,0 +1,23 @@
+package com.miempresa.miapp.ejercicio;
+
+public class APP {
+    public static void main(String[] args)
+    {
+        int numero=17;
+        short marianico=10;
+        long gasol=1980;
+        boolean independencia=false;
+        double presupuesto=22.50;
+        float longitud=16.34f;
+
+        var suma=gasol+marianico;
+
+        System.out.println("int: "+numero);
+        System.out.println("short: "+marianico);
+        System.out.println("long: "+gasol);
+        System.out.println("boolean: "+independencia);
+        System.out.println("double: "+presupuesto);
+        System.out.println("float: "+longitud);
+        System.out.println("Suma: "+suma);
+    }
+}
